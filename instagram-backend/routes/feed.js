3ac5589c8296f02748796fcf1a0f57c276508fcf
Chhavi Sharma <158780605+chhavi0610@users.noreply.github.com>
2025-12-18 22:41:33 +0530

    Update feed.js

--- instagram-backend/routes/feed.js
+++ instagram-backend/routes/feed.js
@@ -3,10 +3,10 @@ const db = require("../db");
 const checkAuth = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.get("/", checkAuth, (req, res) => {
-  const userId = req.userId;
+router.get("/",(req, res) => {
+ 
 const feed = "SELECT posts.id,posts.image_url,posts.caption,posts.created_at,users.username FROM posts JOIN followers ON posts.user_id = followers.following_id JOIN users ON users.id = posts.user_id WHERE followers.follower_id = ? ORDER BY posts.created_at DESC";
-db.query(feed, [userId], (err, result) => {
+db.query(feed,(err, result) => {
 if (err) {
 console.log("FEED SQL ERROR:", err);
       return res.status(500).json({ message: "Error fetching feed" });}
